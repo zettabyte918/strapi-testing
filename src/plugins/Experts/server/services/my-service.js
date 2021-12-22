0b787a42a4812cc3f58ed5767f9a557112793dff
hossem_ubuntu <hammamihossem99@gmail.com>
2021-12-23 00:53:36 +0100

    add comment for future use

--- src/plugins/Experts/server/services/my-service.js
+++ src/plugins/Experts/server/services/my-service.js
@@ -2,7 +2,7 @@
 
 module.exports = ({ strapi }) => ({
   getWelcomeMessage() {
-    return strapi.db.query("plugin::experts.expert").findMany({});
+    return strapi.db.query("plugin::experts.expert").findMany({}); //https://docs.strapi.io/developer-docs/latest/developer-resources/database-apis-reference/query-engine/single-operations.html#findone
   },
   getYolo() {
     const roles = strapi.query("plugin::experts.expert").find();
